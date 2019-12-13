3bbbf7d922df48a4ccd6af4de7563033fd4e0a77
Ethan Lee <ethanlee515@gmail.com>
2019-12-13 12:40:24 +0800

    Finish

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,5 +1,45 @@
-import * as crypto from "crypto"
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
+import * as fs from 'fs'
 
-const id = crypto.randomBytes(16).toString("hex");
+let records = require('./records.json');
 
-console.log(id);
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.options('/', cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.json(records);
+});
+
+app.post('/', (req, res) => {
+	let newRec = req.body;
+	newRec.deleted = false;
+	records.push(newRec);
+	fs.writeFile('records.json', JSON.stringify(records), () => {});
+	res.end();
+});
+
+app.delete('/', (req, res) => {
+	let to_delete = req.body;
+	for(let record of records) {
+		if(record.name == to_delete.name &&
+				record.reason == to_delete.reason &&
+				record.amount == to_delete.amount &&
+				!record.deleted) {
+			record.deleted = true;
+			fs.writeFile('records.json', JSON.stringify(records), () => {});
+			res.end();
+			return;
+		}
+	}
+	res.end();
+});
+
+app.listen(8080, () => {});
--- frontend/tracker.js
+++ frontend/tracker.js
@@ -1,3 +1,5 @@
+const baseURL = 'http://localhost:8080';
+
 //Yes I know these hard coded HTML should really be react components
 function addToRecords(name, amount, reason) {
 	let newRec = $(`
@@ -8,7 +10,13 @@ function addToRecords(name, amount, reason) {
 
 	$(`<button>remove</button>`).appendTo(newRec).click(() => {
 		newRec.remove();
+		$.ajax({
+			url: baseURL,
+			type: 'delete',
+			data: {name, amount, reason}
+		});
 	});
+	return newRec;
 }
 
 $(() => {
@@ -17,5 +25,11 @@ $(() => {
 		let amount = $("#amount").val();
 		let reason = $("#reason").val();
 		addToRecords(name, amount, reason);
+		$.post(baseURL, {name, amount, reason});
+	});
+	$.get(baseURL).then(records => {
+		for(let r of records)
+			if(!r.deleted)
+				addToRecords(r.name, r.amount, r.reason);
 	});
 });
